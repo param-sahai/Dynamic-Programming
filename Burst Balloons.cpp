67457e959ded21ee81ef1313762d55548de7eb86
Param Sahai <74666867+param-sahai@users.noreply.github.com>
2023-08-21 01:11:25 +0530

    Updated Burst Balloons.cpp

--- Burst Balloons.cpp
+++ Burst Balloons.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
-    int helper(int i, int j, vector<int>& nums, vector<vector<int>>&dp){
+    int helper(int i, int j, vector<int>&nums, vector<vector<int>>&dp){
         if(i>j) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        int mx = INT_MIN;
+        int maxCost = INT_MIN;
         for(int k=i;k<=j;k++){
-            int cost = nums[i-1]*nums[k]*nums[j+1] + helper(i, k-1, nums, dp) + helper(k+1, j, nums, dp);
-            mx = max(mx, cost);
+            int cost = (nums[i-1] * nums[k] * nums[j+1]) + helper(i, k-1, nums, dp) + helper(k+1, j, nums, dp);
+            maxCost = max(maxCost, cost);
         }
-         return dp[i][j] = mx;
+        return dp[i][j] = maxCost;
+        
     }
+    
+    
     int maxCoins(vector<int>& nums) {
-        int n=nums.size();
+        int n = nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(), 1);
-        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-        return helper(1, n, nums, dp);
+        vector<vector<int>>dp(n+2, vector<int>(n+2, 0));
+        // return helper(1, n, nums, dp);
+        for(int i=n;i>=1;i--){
+            for(int j=1; j<=n;j++){
+                if(i>j) continue;
+                int maxCost = INT_MIN;
+                for(int k=i;k<=j;k++){
+                    int cost = (nums[i-1] * nums[k] * nums[j+1]) + dp[i][k-1] + dp[k+1][j];
+                    maxCost = max(maxCost, cost);
+                }
+                dp[i][j] = maxCost;
+            }
+        }
+        return dp[1][n];
     }
 };
